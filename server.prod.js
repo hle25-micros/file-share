830157c03e96965ae9adcca40ce0169e3c1136e8
Le Hoang <hle25@dxc.com>
2026-04-12 13:33:43 +0700

    u

--- server.prod.js
+++ server.prod.js
@@ -125,7 +125,8 @@ app.use(helmet({
   crossOriginEmbedderPolicy: false,
 }));
 app.use(cookieParser());
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({limit: '10mb', extends: true}));
 
 // Trust proxy (for rate limiting behind reverse proxy)
 app.set('trust proxy', 1);
