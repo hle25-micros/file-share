b9938907f8398dad3a6f5a2242c657606963c3d4
Le Hoang <hle25@dxc.com>
2026-04-13 10:00:54 +0700

    revert code

--- server.prod.js
+++ server.prod.js
@@ -132,7 +132,7 @@ app.use(helmet({
 }));
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({limit: '10mb', extends: true}));
+app.use(express.urlencoded({limit: '10mb', extended: true}));
 
 // Trust proxy (for rate limiting behind reverse proxy)
 app.set('trust proxy', 1);
